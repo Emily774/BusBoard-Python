a92a41fc211a081fdaec595be23b0d147078e9de
Louise de Mattos <72737315+Emily774@users.noreply.github.com>
2021-09-20 12:27:27 +0100

    added the basic framework for importing and using the api and some ideas to get started in comments

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import requests
-
-r = requests.get('https://api.github.com/events')
+from constants import *
 
 def main():
-    print("Welcome to BusBoard.")
+    atcocode = ''
+    # appId = '2009b387'
+    # appKey = '7b98ba9c457f6c782a48965cdf8f8eb6'
+    r = requests.get(f'http://transportapi.com/v3/uk/bus/stop/{atcoCode}/live.json?app_id={appId}&app_key={appKey}')
+    response = r.json()
+    departures = response["departures"]
+    for busNumber in departures:
+        print(busNumber)
+    for departures in departures[busNumber]:
+        print(departures)
+        depatureTime = departures['expected departure time']
+        
+
+
+# prints json as an array
 
 
 if __name__ == "__main__": main()
